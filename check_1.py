8d0feb80e9cbe92f724b4976f53e615598c984bb
Danny Morton <102087408+dmorton714@users.noreply.github.com>
2022-05-26 08:45:31 -0400

    Add files via upload

---
+++ check_1.py
@@ -0,0 +1,19 @@
+#Knowledge check 1
+
+#a statement that prints "hello world!"
+print("Hello World")
+
+
+#make a list of several values and print one of them. 
+colors = ("red", "blues" , "purple")
+print(colors[2])
+
+#a dictionary populated with two keys and two values. Print one of the values just like you did above
+animals = {}
+animals["dogs"] = "cute"
+animals["sloth"] = "slow" 
+print(animals["dogs"])
+
+#a tuple with 4 values. Print one of them
+unicorn_colors = tuple (("white", "black", "purple", "green"))
+print(unicorn_colors[3])
